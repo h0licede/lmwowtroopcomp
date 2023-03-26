85faa0a8442ebe9e913edaf5f00885ce65190db2
h0licede <38484539+h0licede@users.noreply.github.com>
2023-03-26 14:16:43 -0400

    Update lmcounterbeta.py

--- lmcounterbeta.py
+++ lmcounterbeta.py
@@ -1,4 +1,28 @@
 import streamlit as st
+
+# Set page title
+st.set_page_config(page_title="LORDS MOBILE WOW TROOP COMP")
+
+# Add a title and subtitle
+st.title("LORDS MOBILE WOW TROOP COMP")
+import streamlit as st
+
+
+
+
+
+st.write("")
+st.write("")
+st.write("")
+
+
+
+
+
+
+
+
+import streamlit as st
 import requests
 
 # Define the FormSubmit API endpoint
@@ -8,19 +32,19 @@ def main():
     st.write("<h4 style='text-align: center;'>REGISTER TROOP COMP/FORM COUNTER</h4>", unsafe_allow_html=True)
     st.write("")
 
-    enemy_comp = st.text_input("Enemy Comp", "")
-    enemy_formation = st.selectbox("Enemy Formation", ["Infantry Phalanx", "Ranged Phalanx", "Cavalry Phalanx", "Infantry Wedge", "Ranged Wedge", "Cavalry Wedge"])
-    suggested_comp = st.text_input("Suggested Comp", "")
-    suggested_formation = st.selectbox("Suggested Formation", ["Infantry Phalanx", "Ranged Phalanx", "Cavalry Phalanx", "Infantry Wedge", "Ranged Wedge", "Cavalry Wedge"])
+    input1 = st.text_input("Enemy Comp", "")
+    input2 = st.selectbox("Enemy Formation", ["Infantry Phalanx", "Ranged Phalanx", "Cavalry Phalanx", "Infantry Wedge", "Ranged Wedge", "Cavalry Wedge"])
+    output1 = st.text_input("Suggested Comp", "")
+    output2 = st.selectbox("Suggested Formation", ["Infantry Phalanx", "Ranged Phalanx", "Cavalry Phalanx", "Infantry Wedge", "Ranged Wedge", "Cavalry Wedge"])
     save_button = st.button("Submit")
 
     if save_button:
         # Set the form data as a dictionary
         form_data = {
-            "enemy_comp": enemy_comp,
-            "enemy_formation": enemy_formation,
-            "suggested_comp": suggested_comp,
-            "suggested_formation": suggested_formation,
+            "enemy_comp": input1,
+            "enemy_formation": input2,
+            "suggested_comp": output1,
+            "suggested_formation": output2,
         }
 
         # Send the form data using a POST request
@@ -50,3 +74,60 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+
+import streamlit as st
+
+# Define the sidebar
+sidebar = st.sidebar
+
+# Increase the width of the sidebar
+# Add a title to the sidebar
+sidebar.title("Please report inaccurate troop comp and formation suggestion")
+
+# Add a header to the sidebar
+sidebar.header("Help us improve!")
+sidebar.write("If you find any inaccurate troop comp and formation suggestions, please report it to support@accesstv.live")
+
+# Add a separator to the sidebar
+sidebar.markdown("---")
+
+# Add some additional information to the sidebar
+sidebar.header("Disclaimer")
+  
+sidebar.write("This application serves as a basic reference to enhance your gameplay. However, it's essential to keep in mind that the troop compositions and formations recommended may not always be precise. Additionally, your individual account statistics and your opponent's can significantly impact the outcome of battles, even if you have utilized the recommended combination correctly.")
+
+
+
+
+
+
+
+
+st.write("<h4 style='text-align: center;'>Donation</h4>", unsafe_allow_html=True)
+
+import streamlit as st
+
+st.write("<h4 style='text-align: center;'>If you're feeling generous and want to support my caffeine addiction while I work on this project, consider donating to my coffee fund. I promise I'll use it to stay awake and make more cool stuff 😜</h4>", unsafe_allow_html=True)
+
+
+st.markdown('<div style="text-align:center"><a href="https://paypal.me/h0licede" target="_blank" rel="noopener noreferrer">Please consider donating to my coffee fund to support my work</a></div>', unsafe_allow_html=True)
+
+
+st.write("")
+st.write("")
+st.write("")
+
+st.write("<h4 style='font-size: 14px; text-align: center;'>Join Dark Knights [D!K] guild now! We are looking for experienced rally leaders and T4/T5 fillers to join our team. Be a part of our growing community and conquer the kingdom together.</h4>", unsafe_allow_html=True)
+
+st.write("")
+
+
+
+
+st.markdown(
+    f'<div style="display: flex; justify-content: center;"><img src="https://s2.gifyu.com/images/dark-kinights-white-giffddf999a8d0a6add.gif" width="200"/></div>',
+    unsafe_allow_html=True
+)
