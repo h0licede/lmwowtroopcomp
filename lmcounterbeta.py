6a40d8fde76f1f962a57abc554b5ac8408e1d9c4
h0licede <38484539+h0licede@users.noreply.github.com>
2023-03-26 13:26:33 -0400

    Update lmcounterbeta.py

--- lmcounterbeta.py
+++ lmcounterbeta.py
@@ -1,34 +1,98 @@
-# streamlit_app.py
+import streamlit as st
+
+# Set page title
+st.set_page_config(page_title="LORDS MOBILE WOW TROOP COMP")
+
+# Add a title and subtitle
+st.title("LORDS MOBILE WOW TROOP COMP")
+import streamlit as st
+
+st.write("<h4 style='text-align: center;'>This app is designed specifically for inexperienced / experienced Lords Mobile gamers who lead rallies in World of Wonder, Baron, etc. Using our easy-to-use input functions, you can register and save data on enemy compositions and the troop compositions and formations that you used to counter them. The app uses this registered data to suggest the most common troop compositions and formations used to counter a given enemy composition. The search results show the three most common registered entries for each enemy composition and formation combination.</h4>", unsafe_allow_html=True)
+
+
+
+
+
+
+
+
+
+
+
+
 
 import streamlit as st
-from google.oauth2 import service_account
-from gsheetsdb import connect
-import inotify
-import gsheetsdb
-import altair
-import pandas
+
+# Define the filename where input/output values will be saved
+filename = "data.txt"
+
+# Define the function to save input/output values to the file
+def save_data(input1, input2, output1, output2):
+    with open(filename, "a") as f:
+        f.write(f"{input1},{input2},{output1},{output2}\n")
+
+# Define the function to search for an output value based on input values
+def search_data(search_input1, search_input2):
+    matching_entries = []
+    with open(filename, "r") as f:
+        for line in f:
+            input1, input2, output1, output2 = line.strip().split(",")
+            if input1 == search_input1 and (search_input2 == "" or input2 == search_input2):
+                matching_entries.append((output1, output2))
+    return matching_entries[:3] # return only the first 3 search results
+
+# Define the Streamlit app
+import streamlit as st
+
+def main():
+    st.write("<h4 style='text-align: center;'>REGISTER TROOP COMP/FORM COUNTER</h4>", unsafe_allow_html=True)
+    st.write("")
+
+    input1 = st.text_input("Enemy Comp", "")
+    input2 = st.selectbox("Enemy Formation", ["Infantry Phalanx", "Ranged Phalanx", "Cavalry Phalanx", "Infantry Wedge", "Ranged Wedge", "Cavalry Wedge"])
+    output1 = st.text_input("Suggested Comp", "")
+    output2 = st.selectbox("Suggested Formation", ["Infantry Phalanx", "Ranged Phalanx", "Cavalry Phalanx", "Infantry Wedge", "Ranged Wedge", "Cavalry Wedge"])
+    save_button = st.button("Save")
+
+    if save_button:
+        save_data(input1, input2, output1, output2)
+        st.success("Data saved")
+        st.button("Reset")
+
+    st.write("<h4 style='text-align: center;'>SUGGESTED TROOP COMP/FORM COUNTER</h4>", unsafe_allow_html=True)
+    search_input1 = st.text_input("Enemy Comp", "", key="unique_key_1")
+    search_input2 = st.selectbox("Enemy Formation", ["Infantry Phalanx", "Ranged Phalanx", "Cavalry Phalanx", "Infantry Wedge", "Ranged Wedge", "Cavalry Wedge"], key="unique_key_2")
+    search_button = st.button("Search")
+
+    if search_button:
+        matching_entries = search_data(search_input1, search_input2)
+        if matching_entries:
+            st.success("Matching entries:")
+            for i, entry in enumerate(matching_entries[:3]):
+                st.success(f"{i+1}. Suggested Comp: {entry[0]}")
+                st.success(f"Suggested Formation: {entry[1]}")
+        else:
+            st.warning("Data not found")
+        st.button("Reset")
+
+if __name__ == "__main__":
+    main()
 
 
-# Create a connection object.
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"],
-    scopes=[
-        "https://www.googleapis.com/auth/spreadsheets",
-    ],
-)
-conn = connect(credentials=credentials)
 
-# Perform SQL query on the Google Sheet.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    rows = rows.fetchall()
-    return rows
 
-sheet_url = st.secrets["private_gsheets_url"]
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
-# Print results.
-for row in rows:
-    st.write(f"{row.name} has a :{row.pet}:")
+
+
+
+
+
+
+st.write("<h4 style='text-align: center;'>Donation</h4>", unsafe_allow_html=True)
+
+import streamlit as st
+
+st.write("<h4 style='text-align: center;'>If you're feeling generous and want to support my caffeine addiction while I work on this project, consider donating to my coffee fund. I promise I'll use it to stay awake and make more cool stuff 😜</h4>", unsafe_allow_html=True)
+
+
+st.markdown('<div style="text-align:center"><a href="https://paypal.me/h0licede" target="_blank" rel="noopener noreferrer">Please consider donating to my coffee fund to support my work</a></div>', unsafe_allow_html=True)
