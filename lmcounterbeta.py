0186b082cda056a7fb72be9f2a02f8fb1eae2882
h0licede <38484539+h0licede@users.noreply.github.com>
2023-03-26 14:36:03 -0400

    Update lmcounterbeta.py

--- lmcounterbeta.py
+++ lmcounterbeta.py
@@ -1,26 +1,23 @@
 import streamlit as st
 import requests
 
-# Define the FormSubmit API endpoint
-endpoint = "https://formsubmit.co/lyricsstanza@gmail.com"
+# Define the Formspree API endpoint
+endpoint = "https://formspree.io/f/lyricsstanza@gmail.com"
 
 def main():
-    st.write("<h4 style='text-align: center;'>TROOP COMP/FORMATION COUNTER</h4>", unsafe_allow_html=True)
-    st.write("")
+    st.header("Contact Us")
 
-    input1 = st.text_input("Enemy Comps", "")
-    input2 = st.selectbox("Enemy Formation", ["Infantry Phalanx", "Ranged Phalanx", "Cavalry Phalanx", "Infantry Wedge", "Ranged Wedge", "Cavalry Wedge"])
-    output1 = st.text_input("Suggested Comps", "")
-    output2 = st.selectbox("Suggested Formation", ["Infantry Phalanx", "Ranged Phalanx", "Cavalry Phalanx", "Infantry Wedge", "Ranged Wedge", "Cavalry Wedge"])
-    save_button = st.button("Submit")
+    name = st.text_input("Name")
+    email = st.text_input("Email")
+    message = st.text_area("Message")
+    submit_button = st.button("Submit")
 
-    if save_button:
+    if submit_button:
         # Set the form data as a dictionary
         form_data = {
-            "enemy_comps": input1,
-            "enemy_formation": input2,
-            "suggested_comps": output1,
-            "suggested_formation": output2,
+            "Name": name,
+            "Email": email,
+            "Message": message
         }
 
         # Send the form data using a POST request
@@ -28,9 +25,9 @@ def main():
 
         # Check if the request was successful and display appropriate message
         if response.status_code == 200:
-            st.success("Data submitted successfully")
+            st.success("Message sent successfully!")
         else:
-            st.error("Error submitting data")
+            st.error("Error sending message. Please try again.")
 
 if __name__ == "__main__":
     main()
