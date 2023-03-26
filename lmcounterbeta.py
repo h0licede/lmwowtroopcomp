61ecdda01d37c4f2260f222ad9e534e83d50754d
h0licede <38484539+h0licede@users.noreply.github.com>
2023-03-25 23:48:15 -0400

    Update lmcounterbeta.py

--- lmcounterbeta.py
+++ lmcounterbeta.py
@@ -80,6 +80,31 @@ if __name__ == "__main__":
 
 
 
+import streamlit as st
+import pandas as pd
+
+# Create a table to store user inputs and suggested troop comps and formations
+df = pd.DataFrame(columns=['Enemy Troop Comp', 'Enemy Formation', 'Suggested Troop Comp', 'Suggested Formation'])
+
+# Create a form for users to enter their enemy troop comp and formation
+enemy_troop_comp = st.selectbox('Enemy Troop Comp', ['Infantry Phalanx', 'Ranged Phalanx', 'Cavalry Phalanx', 'Infantry Wedge', 'Ranged Wedge', 'Cavalry Wedge'])
+enemy_formation = st.selectbox('Enemy Formation', ['Infantry Phalanx', 'Ranged Phalanx', 'Cavalry Phalanx', 'Infantry Wedge', 'Ranged Wedge', 'Cavalry Wedge'])
+
+# Define a function to suggest troop comps and formations based on user inputs
+def suggest_troop_comp_and_formation(enemy_troop_comp, enemy_formation):
+    # Insert your algorithm here to suggest troop comps and formations based on user inputs
+    # For now, let's just suggest the same values as the user inputs
+    suggested_troop_comp = enemy_troop_comp
+    suggested_formation = enemy_formation
+    return suggested_troop_comp, suggested_formation
+
+# Add user inputs and suggested troop comps and formations to the table
+if st.button('Add to Table'):
+    suggested_troop_comp, suggested_formation = suggest_troop_comp_and_formation(enemy_troop_comp, enemy_formation)
+    df.loc[len(df)] = [enemy_troop_comp, enemy_formation, suggested_troop_comp, suggested_formation]
+
+# Display the table
+st.dataframe(df)
 
 
 
