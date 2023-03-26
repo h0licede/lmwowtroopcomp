bd2031f0dd5ce98e04cb17349cfbca5d2594e3fd
h0licede <38484539+h0licede@users.noreply.github.com>
2023-03-25 23:48:49 -0400

    Update lmcounterbeta.py

--- lmcounterbeta.py
+++ lmcounterbeta.py
@@ -16,70 +16,6 @@ st.write("<h4 style='text-align: center;'>This app is designed specifically for 
 
 
 
-
-
-
-
-
-import streamlit as st
-
-# Define the filename where input/output values will be saved
-filename = "data.txt"
-
-# Define the function to save input/output values to the file
-def save_data(input1, input2, output1, output2):
-    with open(filename, "a") as f:
-        f.write(f"{input1},{input2},{output1},{output2}\n")
-
-# Define the function to search for an output value based on input values
-def search_data(search_input1, search_input2):
-    matching_entries = []
-    with open(filename, "r") as f:
-        for line in f:
-            input1, input2, output1, output2 = line.strip().split(",")
-            if input1 == search_input1 and (search_input2 == "" or input2 == search_input2):
-                matching_entries.append((output1, output2))
-    return matching_entries[:3] # return only the first 3 search results
-
-# Define the Streamlit app
-import streamlit as st
-
-def main():
-    st.write("<h4 style='text-align: center;'>REGISTER TROOP COMP/FORM COUNTER</h4>", unsafe_allow_html=True)
-    st.write("")
-
-    input1 = st.text_input("Enemy Comp", "")
-    input2 = st.selectbox("Enemy Formation", ["Infantry Phalanx", "Ranged Phalanx", "Cavalry Phalanx", "Infantry Wedge", "Ranged Wedge", "Cavalry Wedge"])
-    output1 = st.text_input("Suggested Comp", "")
-    output2 = st.selectbox("Suggested Formation", ["Infantry Phalanx", "Ranged Phalanx", "Cavalry Phalanx", "Infantry Wedge", "Ranged Wedge", "Cavalry Wedge"])
-    save_button = st.button("Save")
-
-    if save_button:
-        save_data(input1, input2, output1, output2)
-        st.success("Data saved")
-        st.button("Reset")
-
-    st.write("<h4 style='text-align: center;'>SUGGESTED TROOP COMP/FORM COUNTER</h4>", unsafe_allow_html=True)
-    search_input1 = st.text_input("Enemy Comp", "", key="unique_key_1")
-    search_input2 = st.selectbox("Enemy Formation", ["Infantry Phalanx", "Ranged Phalanx", "Cavalry Phalanx", "Infantry Wedge", "Ranged Wedge", "Cavalry Wedge"], key="unique_key_2")
-    search_button = st.button("Search")
-
-    if search_button:
-        matching_entries = search_data(search_input1, search_input2)
-        if matching_entries:
-            st.success("Matching entries:")
-            for i, entry in enumerate(matching_entries[:3]):
-                st.success(f"{i+1}. Suggested Comp: {entry[0]}")
-                st.success(f"Suggested Formation: {entry[1]}")
-        else:
-            st.warning("Data not found")
-        st.button("Reset")
-
-if __name__ == "__main__":
-    main()
-
-
-
 import streamlit as st
 import pandas as pd
 
@@ -105,6 +41,7 @@ if st.button('Add to Table'):
 
 # Display the table
 st.dataframe(df)
+
 
 
 
