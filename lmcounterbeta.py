c55ee7c09c67390bc57b2603350a71685782a39d
h0licede <38484539+h0licede@users.noreply.github.com>
2023-03-26 15:15:48 -0400

    Update lmcounterbeta.py

--- lmcounterbeta.py
+++ lmcounterbeta.py
@@ -43,38 +43,42 @@ if __name__ == "__main__":
     
 import streamlit as st
 
-def main():
-    st.header("Troop Suggestions")
+# Read data from text file
+data_file = "data.txt"
+data = []
+with open(data_file, "r") as f:
+    for line in f.readlines():
+        comps = line.strip().split(",")
+        data.append({
+            "enemy_comp": comps[0],
+            "enemy_form": comps[1],
+            "suggested_comp": comps[2],
+            "suggested_form": comps[3]
+        })
 
-    # Read data from the data file
-    with open("data.txt", "r") as f:
-        data = f.readlines()
+# Define function for searching
+def search():
+    enemy_comp = st.text_input("Enter enemy composition:")
+    enemy_form = st.selectbox("Select enemy formation:", ["Infantry Phalanx", "Ranged Phalanx", "Cavalry Phalanx", "Infantry Wedge", "Ranged Wedge", "Cavalry Wedge"])
 
-    # Create a dictionary to store the data
-    troop_data = {}
-    for entry in data:
-        entry = entry.strip().split(",")
-        enemy_comp = entry[0]
-        enemy_form = entry[1]
-        suggested_comp = entry[2]
-        suggested_form = entry[3]
-        troop_data[(enemy_comp, enemy_form)] = (suggested_comp, suggested_form)
+    # Filter data based on user input
+    results = [d for d in data if d["enemy_comp"] == enemy_comp and d["enemy_form"] == enemy_form]
 
-    # Create the search form
-    st.subheader("Search Enemy Troop Composition and Formation")
-    enemy_comp = st.number_input("Enter Enemy Troop Composition (3-digit number)", min_value=0, max_value=999, step=1)
-    enemy_form = st.selectbox("Enter Enemy Troop Formation", ["Infantry Phalanx", "Ranged Phalanx", "Cavalry Phalanx", "Infantry Wedge", "Ranged Wedge", "Cavalry Wedge"])
-
-    # Get the suggested troop composition and formation
-    suggested_comp, suggested_form = troop_data.get((enemy_comp, enemy_form), ("", ""))
-
-    # Display the result
-    if suggested_comp and suggested_form:
-        st.subheader("Suggested Troop Composition and Formation")
-        st.write(f"Troop Composition: {suggested_comp}")
-        st.write(f"Troop Formation: {suggested_form}")
+    # Display results
+    if results:
+        st.write("Suggested Troop Composition and Formation:")
+        for result in results:
+            st.write(f"{result['suggested_comp']} {result['suggested_form']}")
     else:
-        st.subheader("No Matching Troop Data Found")
+        st.write("No results found.")
+
+# Create Streamlit app
+def app():
+    st.title("Troop Suggestion Tool")
+
+    search_button = st.button("Search")
+    if search_button:
+        search()
 
 if __name__ == "__main__":
-    main()
+    app()
