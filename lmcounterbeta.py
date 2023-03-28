b4ae35fadc8855ed289a6aa4caf2149d945ffdea
h0licede <38484539+h0licede@users.noreply.github.com>
2023-03-27 21:46:57 -0400

    Update lmcounterbeta.py

--- lmcounterbeta.py
+++ lmcounterbeta.py
@@ -2,29 +2,20 @@ import streamlit as st
 import pandas as pd
 
 # Load the data
-data = {'Enemy Comp': ['424, 947, 857', '442, 992', '442, 424 (All variants), 992', '442, 992, 208, 055', '749, 659, 424, 055', 'All 424 variants, 208'],
-        'Suggested Comp': ['749', '947', '208', '992', '442', '055'],
-        'Suggested Comp Can Be Countered By': ['424, 749, 208, 055', '(992, 442, 424 variants) - Must be in ranged wedge and 055 is good to snipe 208.', '424, 749, 758, 659, 055', '947, 523, Inf Blast, 208, 974, 866', '208, 947, 523, 974, 866', '992, 442, 866, 974']}
-df = pd.DataFrame(data)
+df = pd.read_csv('comps.txt', sep='\t')
 
-# Set page title
-st.set_page_config(page_title="Troop Comp Counter Bot")
-
-# Display title
-st.title("Troop Comp Counter Bot")
-
-# Get user input
-user_input = st.text_input("Type in enemy comp in 3 digit number:")
-
-# Display results
-if user_input:
+# Define the message handler
+def echo():
+    user_input = st.text_input("Type in enemy comp in 3 digit number:")
     found = False
     for index, row in df.iterrows():
         if user_input in row['Enemy Comp'] or user_input == row['Suggested Comp'] or user_input in row['Suggested Comp Can Be Countered By']:
             found = True
-            st.write("Enemy Comp:", row['Enemy Comp'])
-            st.write("Suggested Comp:", row['Suggested Comp'])
-            st.write("Suggested Comp Can Be Countered By:", row['Suggested Comp Can Be Countered By'])
+            message = f"Enemy Comp: {row['Enemy Comp']}\nSuggested Comp: {row['Suggested Comp']}\nSuggested Comp Can Be Countered By: {row['Suggested Comp Can Be Countered By']}"
+            st.write(message)
             break
     if not found:
         st.write("No matching troop comp found. Please enter a valid 3 digit number.")
+
+# Run the app
+echo()
