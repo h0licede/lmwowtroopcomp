e4a1135ee9a5f09fd3bd6f83dcfc65cc68880d8a
h0licede <38484539+h0licede@users.noreply.github.com>
2023-03-27 22:16:50 -0400

    Update lmcounterbeta.py

--- lmcounterbeta.py
+++ lmcounterbeta.py
@@ -2,22 +2,21 @@ import streamlit as st
 import pandas as pd
 
 # Load the data
-df = pd.read_csv('comps.csv')
+comps = pd.read_csv('comps.csv')
 
-# Define the message handler
-def echo():
-    user_input = st.text_input('Type in enemy comp in 3 digit number:')
+# Define the app
+def app():
+    # Set up the sidebar
+    st.sidebar.title("Troop Comp Counter Bot")
+    user_input = st.sidebar.text_input("Type in enemy comp in 3 digit number:")
+
+    # Show the results
     if user_input:
         found = False
-        for index, row in df.iterrows():
+        for index, row in comps.iterrows():
             if user_input in row['Enemy Comp'] or user_input == row['Suggested Comp'] or user_input in row['Suggested Comp Can Be Countered By']:
                 found = True
-                message = f"Enemy Comp: {row['Enemy Comp']}\nSuggested Comp: {row['Suggested Comp']}\nSuggested Comp Can Be Countered By: {row['Suggested Comp Can Be Countered By'].split(',')}"
+                message = f"Enemy Comp: {row['Enemy Comp']}\nSuggested Comp: {row['Suggested Comp']}\nSuggested Comp Can Be Countered By: {row['Suggested Comp Can Be Countered By']}"
                 st.write(message)
-                break
         if not found:
             st.write("No matching troop comp found. Please enter a valid 3 digit number.")
-
-# Set up the app
-st.title('Troop Comp Counter Bot')
-echo()
