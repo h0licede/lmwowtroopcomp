490b199e20153d1842ac84847e0e6aa64a5baf85
h0licede <38484539+h0licede@users.noreply.github.com>
2023-03-27 21:51:46 -0400

    Update lmcounterbeta.py

--- lmcounterbeta.py
+++ lmcounterbeta.py
@@ -2,20 +2,23 @@ import streamlit as st
 import pandas as pd
 
 # Load the data
-df = pd.read_csv('comps.txt', sep='\t')
+df = pd.read_csv("comps.txt")
 
 # Define the message handler
-def echo():
-    user_input = st.text_input("Type in enemy comp in 3 digit number:")
+def echo(user_input):
     found = False
     for index, row in df.iterrows():
         if user_input in row['Enemy Comp'] or user_input == row['Suggested Comp'] or user_input in row['Suggested Comp Can Be Countered By']:
             found = True
             message = f"Enemy Comp: {row['Enemy Comp']}\nSuggested Comp: {row['Suggested Comp']}\nSuggested Comp Can Be Countered By: {row['Suggested Comp Can Be Countered By']}"
-            st.write(message)
+            return message
             break
     if not found:
-        st.write("No matching troop comp found. Please enter a valid 3 digit number.")
+        return "No matching troop comp found. Please enter a valid 3 digit number."
 
-# Run the app
-echo()
+# Set up the app
+st.title("Troop Comp Counter")
+user_input = st.text_input("Type in enemy comp in 3 digit number:")
+if user_input:
+    message = echo(user_input)
+    st.write(message)
