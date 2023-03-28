2a21c99b9cbe5609381faf3de71a73466eef719f
h0licede <38484539+h0licede@users.noreply.github.com>
2023-03-27 22:21:09 -0400

    Update lmcounterbeta.py

--- lmcounterbeta.py
+++ lmcounterbeta.py
@@ -18,5 +18,6 @@ def app():
                 found = True
                 message = f"Enemy Comp: {row['Enemy Comp']}\nSuggested Comp: {row['Suggested Comp']}\nSuggested Comp Can Be Countered By: {row['Suggested Comp Can Be Countered By']}"
                 st.write(message)
+                break
         if not found:
             st.write("No matching troop comp found. Please enter a valid 3 digit number.")
