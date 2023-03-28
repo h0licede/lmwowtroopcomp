833a34159e17ab5c103b82d381c272f06a0f77c5
h0licede <38484539+h0licede@users.noreply.github.com>
2023-03-27 22:21:47 -0400

    Update lmcounterbeta.py

--- lmcounterbeta.py
+++ lmcounterbeta.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 # Load the data
 comps = pd.read_csv('comps.csv')
+comps.columns = comps.columns.str.strip()
 
 # Define the app
 def app():
@@ -18,6 +19,5 @@ def app():
                 found = True
                 message = f"Enemy Comp: {row['Enemy Comp']}\nSuggested Comp: {row['Suggested Comp']}\nSuggested Comp Can Be Countered By: {row['Suggested Comp Can Be Countered By']}"
                 st.write(message)
-                break
         if not found:
             st.write("No matching troop comp found. Please enter a valid 3 digit number.")
