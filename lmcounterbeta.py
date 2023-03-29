43aa4d784edaee98d5e8e84e12b86fb71aa2d1ae
h0licede <38484539+h0licede@users.noreply.github.com>
2023-03-28 21:50:05 -0400

    Update lmcounterbeta.py

--- lmcounterbeta.py
+++ lmcounterbeta.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import requests
 
 # Load the data
 comps = pd.read_csv('comps.csv')
@@ -10,15 +11,15 @@ def app():
 
     # Show the results
     if user_input:
-        row = comps[comps['Enemy Comp'] == user_input]
-        if not row.empty:
-            message = f"Enemy Comp: {row['Enemy Comp'].values[0]}\nCounter Comp: {row['Counter Comp'].values[0]}"
-            st.write(message)
-        else:
+        found = False
+        for index, row in comps.iterrows():
+            if user_input in row['Enemy Comp']:
+                found = True
+                message = f"Enemy Comp: {row['Enemy Comp']}\nCounter Comp: {row['Counter Comp']}"
+                st.write(message)
+        if not found:
             st.write("No matching troop comp found. Please enter a valid 3 digit number.")
 
-# Run the app
-app()
 
 
 
