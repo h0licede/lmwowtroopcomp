0e4161877e0653e380239cd234831e9d8b1efaaf
h0licede <38484539+h0licede@users.noreply.github.com>
2023-03-28 21:54:47 -0400

    Update lmcounterbeta.py

--- lmcounterbeta.py
+++ lmcounterbeta.py
@@ -1,19 +1,18 @@
 import streamlit as st
 import pandas as pd
-import requests
 
 # Load the data
 comps = pd.read_csv('comps.csv')
 
 # Define the app
 def app():
-    user_input = st.text_input("Type in troop comp in 3 digit number:")
+    user_input = st.text_input("Type in enemy troop comp in 3 digit number:")
 
     # Show the results
     if user_input:
         found = False
         for index, row in comps.iterrows():
-            if str(user_input) in row['Enemy Comp']:
+            if str(user_input) in str(row['Enemy Comp']):
                 found = True
                 message = f"Enemy Comp: {row['Enemy Comp']}\nCounter Comp: {row['Counter Comp']}"
                 st.write(message)
@@ -22,6 +21,7 @@ def app():
 
 # Run the app
 app()
+
 
 
 
