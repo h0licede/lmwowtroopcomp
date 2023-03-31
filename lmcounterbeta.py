06c58529321352e821541a684f8e41fbd9aeac9c
h0licede <38484539+h0licede@users.noreply.github.com>
2023-03-30 22:16:07 -0400

    Update lmcounterbeta.py

--- lmcounterbeta.py
+++ lmcounterbeta.py
@@ -167,46 +167,38 @@ sidebar.write("<p style='font-size: 14px;'>This application serves as a basic re
 
 
 
-import streamlit as st
 import pandas as pd
-from pathlib import Path
+import streamlit as st
 
-# create a path to the csv file
-csv_path = Path("suggested_comps.csv")
+# Load the suggested comps data
+suggested_comps = pd.read_csv("suggested_comps.csv", index_col=0)
 
-# create an empty DataFrame or read the existing one
-if csv_path.exists():
-    suggested_comps = pd.read_csv(csv_path)
-else:
-    suggested_comps = pd.DataFrame(columns=["Enemy Comp", "Counter Comp"])
+# Define the layout of the app
+st.title("Suggested Enemy Comp and Counter Comp")
+enemy_comp = st.text_input("Suggested Enemy Comp (3 digit code)")
+counter_comp = st.text_input("Suggested Counter Comp (3 digit code)")
 
-# display the current entries
-st.write(suggested_comps)
-
-# get user input
-enemy_comp = st.text_input("Suggested Enemy Comp (3-digit code)")
-counter_comp = st.text_input("Suggested Counter Comp (3-digit code)")
-
-# check if input is valid
-valid_input = True
-if len(enemy_comp) != 3 or not enemy_comp.isdigit():
-    st.error("Invalid Enemy Comp: Please enter a 3-digit number")
-    valid_input = False
-if len(counter_comp) != 3 or not counter_comp.isdigit():
-    st.error("Invalid Counter Comp: Please enter a 3-digit number")
-    valid_input = False
-
-if valid_input:
-    # check if the entry already exists
-    if ((suggested_comps["Enemy Comp"] == enemy_comp) & (suggested_comps["Counter Comp"] == counter_comp)).any():
-        st.write("Entry already exists!")
-        suggested_comps.loc[(suggested_comps["Enemy Comp"] == enemy_comp) & (suggested_comps["Counter Comp"] == counter_comp), "Counter Comp"] = counter_comp
-        st.write(f"Overwrote {sum((suggested_comps['Enemy Comp'] == enemy_comp) & (suggested_comps['Counter Comp'] == counter_comp))} duplicate entry/ies")
+# Handle the submission of the form
+if st.button("Submit"):
+    # Check if the entry already exists in the dataframe
+    existing_entries = suggested_comps.loc[(suggested_comps["Enemy Comp"] == enemy_comp) & 
+                                            (suggested_comps["Counter Comp"] == counter_comp)]
+    
+    if not existing_entries.empty:
+        # If the entry already exists, merge the data
+        existing_entries["Count"] += 1
+        suggested_comps.update(existing_entries)
+        st.success("Entry updated successfully!")
     else:
-        # add the new entry to the DataFrame
-        suggested_comps = suggested_comps.append({"Enemy Comp": enemy_comp, "Counter Comp": counter_comp}, ignore_index=True)
-        st.write("Entry added!")
+        # If the entry does not exist, add it to the dataframe
+        new_entry = pd.DataFrame({"Enemy Comp": [enemy_comp], "Counter Comp": [counter_comp], "Count": [1]})
+        suggested_comps = pd.concat([suggested_comps, new_entry], ignore_index=True)
+        st.success("Entry added successfully!")
 
-    # save the DataFrame to csv
-    suggested_comps.to_csv(csv_path, index=False)
+    # Save the updated suggested comps data to a file
+    suggested_comps.to_csv("suggested_comps.csv")
+
+# Display the table of suggested comps
+st.write("Suggested Comps:")
+st.table(suggested_comps)
 
